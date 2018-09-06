6a8c1173463dfe54b06b5bd7ec8fd1d5c381c9e9
Daniele Acquaviva <danieleacquaviva5@gmail.com>
2018-09-06 04:46:18 +0200

    Completato log in

--- app/src/main/java/gruppo_20/iassistant/LoginActivity.java
+++ app/src/main/java/gruppo_20/iassistant/LoginActivity.java
@@ -1,13 +1,92 @@
 package gruppo_20.iassistant;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText emailView;
+    private EditText pswView;
+    private Button logButton;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailView = (EditText)findViewById(R.id.emailText);
+        pswView = (EditText)findViewById(R.id.passwordText);
+        logButton = (Button)findViewById(R.id.logButton);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+        if (mAuth.getCurrentUser() != null) {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            finish();
+        }
+
+
+        logButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailView.getText().toString();
+                final String password = pswView.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Inserire Email!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Inserire password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                //authenticate user
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+
+                                if (!task.isSuccessful()) {
+                                    // there was an error
+
+                                        Toast.makeText(LoginActivity.this, "Autenticazione fallita", Toast.LENGTH_LONG).show();
+
+                                } else {
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    intent.putExtra("user",user.getEmail());
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            }
+                        });
+
+
+            }
+        });
     }
+
+
 }
--- app/src/main/java/gruppo_20/iassistant/MainActivity.java
+++ app/src/main/java/gruppo_20/iassistant/MainActivity.java
@@ -1,5 +1,6 @@
 package gruppo_20.iassistant;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,9 +13,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+        private TextView emailUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +44,12 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        Intent intent = getIntent();
+        String email = intent.getStringExtra("user");
+        emailUser = (TextView)findViewById(R.id.emailUser);
+        emailUser.setText(email);
+
     }
 
     @Override

498b332edf218a6ad22c85510e4295d2ff6f28a4
Daniele Acquaviva <danieleacquaviva5@gmail.com>
2018-09-06 03:23:11 +0200

    Aggiunta login Activity

---
+++ app/src/main/java/gruppo_20/iassistant/LoginActivity.java
@@ -0,0 +1,13 @@
+package gruppo_20.iassistant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

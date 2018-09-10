3982a0b073738ffea459919afceae0f684fa9e94
vito <vitopalmisano@gmail.com>
2018-09-10 22:55:29 +0200

    Crezione dell'Activity Prestazioni

--- app/src/main/java/gruppo_20/iassistant/ui/MainActivity.java
+++ app/src/main/java/gruppo_20/iassistant/ui/MainActivity.java
@@ -283,7 +283,8 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_myAccount) {
 
         } else if (id == R.id.nav_associatedDevices) {
-
+            startActivity(new Intent(MainActivity.this, PrestazioniActivity.class));
+            finish();
         } else if (id == R.id.nav_logOut) {
             FirebaseAuth.getInstance().signOut();
             startActivity(new Intent(MainActivity.this, LoginActivity.class));
---
+++ app/src/main/java/gruppo_20/iassistant/ui/PrestazioniActivity.java
@@ -0,0 +1,15 @@
+package gruppo_20.iassistant.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import gruppo_20.iassistant.R;
+
+public class PrestazioniActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prestazioni);
+    }
+}

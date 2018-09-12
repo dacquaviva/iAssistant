496f761a37a6937d29f4c67e35f4f31eb934a231
frabatx <f.battista12@studenti.uniba.it>
2018-09-12 13:26:46 +0200

    Aggiunta Dialog bluetooth

--- app/src/main/java/gruppo_20/iassistant/ui/PrestazioniActivity.java
+++ app/src/main/java/gruppo_20/iassistant/ui/PrestazioniActivity.java
@@ -1,5 +1,7 @@
 package gruppo_20.iassistant.ui;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.Icon;
@@ -18,6 +20,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -32,6 +35,7 @@ public class PrestazioniActivity extends AppCompatActivity {
     private static FloatingActionButton fab;
     private static RecyclerView prestazioniList;
     private static android.support.design.bottomappbar.BottomAppBar but;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,6 +80,8 @@ public class PrestazioniActivity extends AppCompatActivity {
         });
     }
 
+
+
     //definizione del metodo per il riempimento della lista
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) { //@NonNull specifica che il metodo non potrà mai restituire null
         recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(this, Prestazioni.ITEMS));
@@ -106,8 +112,25 @@ public class PrestazioniActivity extends AppCompatActivity {
         public void onBindViewHolder(final SimpleItemRecyclerViewAdapter.ViewHolder holder, final int position) {
             holder.mNomePrestazione.setText(mValues.get(position).nomePrestazione);
             holder.mNumPrestazione.setText(mValues.get(position).numeroPrestazione);
+            holder.mBluetooth.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    apriDialog(holder.blueDialog,"bluetooth",v, R.layout.bluetooth);
+                }
+            });
+
         }
 
+        public void apriDialog(Dialog MyDialog, String title,View v, int layout){
+            MyDialog = new Dialog(v.getContext());
+            MyDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            MyDialog.setContentView(layout);
+            MyDialog.setTitle(title);
+
+            MyDialog.show();
+
+
+        }
 
         @Override
         public int getItemCount() {
@@ -117,11 +140,18 @@ public class PrestazioniActivity extends AppCompatActivity {
         class ViewHolder extends RecyclerView.ViewHolder {
             final Button mNumPrestazione;
             final TextView mNomePrestazione;
+            final FloatingActionButton mBluetooth;
+            final FloatingActionButton mManuale;
+            Dialog blueDialog;
+            Dialog manualDialog;
+
 
             ViewHolder(View view) {
                 super(view);
                 mNumPrestazione = (Button) view.findViewById(R.id.numeroPrestazioni);
                 mNomePrestazione = (TextView) view.findViewById(R.id.nomePrestazione);
+                mBluetooth = (FloatingActionButton) view.findViewById(R.id.bluetoothFab);
+                mManuale = (FloatingActionButton) view.findViewById(R.id.manualFab);
             }
         }
     }

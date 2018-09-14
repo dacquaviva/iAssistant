76093e55b1591750bc4a9506286f45df5737d3d3
Daniele Acquaviva <danieleacquaviva5@gmail.com>
2018-09-14 17:42:09 +0200

    Aggiunta funzionalita' per la corretta ricezioni dei dati, tramite bluetooth

--- app/src/main/java/gruppo_20/iassistant/ui/PrestazioniActivity.java
+++ app/src/main/java/gruppo_20/iassistant/ui/PrestazioniActivity.java
@@ -1,6 +1,12 @@
 package gruppo_20.iassistant.ui;
 
 import android.app.Dialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.design.bottomappbar.BottomAppBar;
 import android.support.design.chip.Chip;
@@ -17,6 +23,7 @@ import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -27,7 +34,13 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 import gruppo_20.iassistant.R;
 import gruppo_20.iassistant.model.Prestazione;
@@ -52,8 +65,58 @@ public class PrestazioniActivity extends AppCompatActivity {
     private static Dialog inserimentoBlueBialog;
 
     private DatabaseReference dbRefVisita;
-    private String idOperatore = FirebaseAuth.getInstance().getUid();;
+    private String idOperatore = FirebaseAuth.getInstance().getUid();
 
+    //Button listen,send, listDevices;
+    private static TextView stato,valorOttenutoBlu;
+
+    private static BluetoothAdapter bluetoothAdapter;
+    private static BluetoothDevice[] btArray;
+    private static ArrayList<String> dati;
+
+
+    private static SendReceive sendReceive;
+
+    static final int STATE_LISTENING = 1;
+    static final int STATE_CONNECTING=2;
+    static final int STATE_CONNECTED=3;
+    static final int STATE_CONNECTION_FAILED=4;
+    static final int STATE_MESSAGE_RECEIVED=5;
+
+    int REQUEST_ENABLE_BLUETOOTH=1;
+
+    private static final String APP_NAME = "iAssistant";
+    private static final UUID MY_UUID=UUID.fromString("8ce255c0-223a-11e0-ac64-0803450c9a66");
+
+    private static Handler handler=new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+
+            switch (msg.what)
+            {
+                case STATE_LISTENING:
+                    stato.setText("Listening");
+                    break;
+                case STATE_CONNECTING:
+                    stato.setText("Connecting");
+                    break;
+                case STATE_CONNECTED:
+                    stato.setText("Connected");
+                    break;
+                case STATE_CONNECTION_FAILED:
+                    stato.setText("Connection Failed");
+                    break;
+                case STATE_MESSAGE_RECEIVED:
+                    byte[] readBuff= (byte[]) msg.obj;
+                    String tempMsg=new String(readBuff,0,msg.arg1);
+                    dati = new ArrayList<>();
+                    valorOttenutoBlu.setText(tempMsg);
+                    dati.add(tempMsg);
+                    break;
+            }
+            return true;
+        }
+    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,6 +128,15 @@ public class PrestazioniActivity extends AppCompatActivity {
         cognomeNomePaziente = getIntent().getExtras().getString("cognomeNomePaziente");
         dbRefVisita = FirebaseDatabase.getInstance().getReference().child("operatori").child(idOperatore).child("visite").child(dataVisita).child(orarioVisita);
         prestazioniList = (RecyclerView) findViewById(R.id.prestazioni_list);
+
+        // Inizializzazione bluetooth
+        bluetoothAdapter=BluetoothAdapter.getDefaultAdapter();
+        //Verifica delle presenza del bluetooth
+        if(!bluetoothAdapter.isEnabled())
+        {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent,REQUEST_ENABLE_BLUETOOTH);
+        }
 
         //Riempimento Header con i dati del paziente
         TextView nomePaziente = (TextView)  findViewById(R.id.headPrestazioni_nomePazienteTextView);
@@ -175,34 +247,62 @@ public class PrestazioniActivity extends AppCompatActivity {
                             blueDialogList.setTitle("Bluetooth");
                             blueDialogList.setContentView(R.layout.bluetooth);
                             listaDispositivi = (ListView) blueDialogList.findViewById(R.id.lista_dispositivi);
-                            String[] dati = {"uno","due"};
-                            ArrayAdapter<String> ciao = new ArrayAdapter<String>(v.getContext(),android.R.layout.simple_list_item_1,dati);
-                            listaDispositivi.setAdapter(ciao);
+                            Set<BluetoothDevice> bt=bluetoothAdapter.getBondedDevices();
+                            String[] strings=new String[bt.size()];
+                            btArray=new BluetoothDevice[bt.size()];
+                            int index=0;
+
+                            if( bt.size()>0)
+                            {
+                                for(BluetoothDevice device : bt)
+                                {
+                                    btArray[index]= device;
+                                    strings[index]=device.getName();
+                                    index++;
+                                }
+                                ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(v.getContext(),android.R.layout.simple_list_item_1,strings);
+                                listaDispositivi.setAdapter(arrayAdapter);
+                            }
+
                             listaDispositivi.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                                 @Override
                                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                                    blueDialogList.cancel();
-                                    inserimentoBlueBialog = new Dialog(view.getContext());
-                                    inserimentoBlueBialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-                                    inserimentoBlueBialog.setTitle("Bluetooth");
-                                    inserimentoBlueBialog.setContentView(R.layout.inserimento_dati_bluetooth);
+                                    stato = (TextView) blueDialogList.findViewById(R.id.stato);
+                                    stato.setText("Connessione");
 
-                                    Button conferma = (Button) inserimentoBlueBialog.findViewById(R.id.button_conferma_blu);
-                                    Button riesegui = (Button) inserimentoBlueBialog.findViewById(R.id.button_riesegui);
 
-                                    conferma.setOnClickListener(new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View v) {
-                                            //gestire conferma dati inseriti dall'operatore
-                                        }
-                                    });
-                                    riesegui.setOnClickListener(new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View v) {
-                                            //gestire annulla
-                                        }
-                                    });
-                                    inserimentoBlueBialog.show();
+                                        blueDialogList.cancel();
+                                        inserimentoBlueBialog = new Dialog(view.getContext());
+                                        inserimentoBlueBialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                                        inserimentoBlueBialog.setTitle("Bluetooth");
+                                        inserimentoBlueBialog.setContentView(R.layout.inserimento_dati_bluetooth);
+                                        stato = (TextView) blueDialogList.findViewById(R.id.stato);
+
+                                        Button conferma = (Button) inserimentoBlueBialog.findViewById(R.id.button_conferma_blu);
+                                        Button annulla = (Button) inserimentoBlueBialog.findViewById(R.id.button_riesegui);
+                                        valorOttenutoBlu = (TextView) inserimentoBlueBialog.findViewById(R.id.valorOttenutoBlu);
+                                        EditText noteInserite = (EditText) inserimentoBlueBialog.findViewById(R.id.noteInserite);
+
+                                        //TODO ANGELO
+                                        conferma.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                             // salvare nel database L'arrayList chiamao dati,e l'EditText note inserite controllare se e' vuoto
+                                            }
+                                        });
+                                        annulla.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                                inserimentoBlueBialog.cancel();
+                                            }
+                                        });
+                                        ClientClass clientClass=new ClientClass(btArray[position]);
+                                        clientClass.start();
+
+                                        stato.setText("Connesione");
+                                        inserimentoBlueBialog.show();
+
+
                                 }
                             });
 
@@ -221,17 +321,20 @@ public class PrestazioniActivity extends AppCompatActivity {
 
                             Button conferma = (Button) inserimentoDialog.findViewById(R.id.conferma_button_manual);
                             Button annulla = (Button) inserimentoDialog.findViewById(R.id.button_annulla);
+                            TextView dato = (TextView)  inserimentoDialog.findViewById(R.id.datoInserito);
+                            EditText noteInserite = (EditText)  inserimentoDialog.findViewById(R.id.noteInserite);
 
+                            //TODO ANGELO
                             conferma.setOnClickListener(new View.OnClickListener() {
                                 @Override
                                 public void onClick(View v) {
-                                    //gestire conferma dati inseriti dall'operatore
+                                    // salvare nel database il valore contenuto nella TextView dato, e nella EditText noteInserite controlllare se sono presenti
                                 }
                             });
                             annulla.setOnClickListener(new View.OnClickListener() {
                                 @Override
                                 public void onClick(View v) {
-                                    //gestire annulla
+                                    inserimentoDialog.cancel();
                                 }
                             });
                             inserimentoDialog.show();
@@ -254,6 +357,83 @@ public class PrestazioniActivity extends AppCompatActivity {
                 super(view);
                 mNumPrestazione = (Button) view.findViewById(R.id.numeroPrestazione);
                 mNomePrestazione = (TextView) view.findViewById(R.id.nomePrestazione);
+            }
+        }
+    }
+
+
+    private static class ClientClass extends Thread
+    {
+        private BluetoothDevice device;
+        private BluetoothSocket socket;
+
+        public ClientClass (BluetoothDevice device1)
+        {
+            device=device1;
+
+            try {
+                socket=device.createRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void run()
+        {
+            try {
+                socket.connect();
+                Message message=Message.obtain();
+                message.what=STATE_CONNECTED;
+                handler.sendMessage(message);
+
+                sendReceive=new SendReceive(socket);
+                sendReceive.start();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                Message message=Message.obtain();
+                message.what=STATE_CONNECTION_FAILED;
+                handler.sendMessage(message);
+            }
+        }
+    }
+
+    private static  class SendReceive extends Thread
+    {
+        private final BluetoothSocket bluetoothSocket;
+        private final InputStream inputStream;
+        private final OutputStream outputStream;
+
+        public SendReceive (BluetoothSocket socket)
+        {
+            bluetoothSocket=socket;
+            InputStream tempIn=null;
+            OutputStream tempOut=null;
+
+            try {
+                tempIn=bluetoothSocket.getInputStream();
+                tempOut=bluetoothSocket.getOutputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            inputStream=tempIn;
+            outputStream=tempOut;
+        }
+
+        public void run()
+        {
+            byte[] buffer=new byte[1024];
+            int bytes;
+
+            while (true)
+            {
+                try {
+                    bytes=inputStream.read(buffer);
+                    handler.obtainMessage(STATE_MESSAGE_RECEIVED,bytes,-1,buffer).sendToTarget();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
